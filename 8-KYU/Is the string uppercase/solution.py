797cdd844a60bd75354f75f92c1416c2b3768ee7
Devansh Batham <devanshbatham009@gmail.com>
2019-11-26 23:10:12 +0530

    Create solution.py

---
+++ 8-KYU/Is the string uppercase/solution.py
@@ -0,0 +1,9 @@
+'''
+challenge URL : https://www.codewars.com/kata/56cd44e1aa4ac7879200010b
+'''
+
+def is_uppercase(inp):
+    if inp.upper() == inp:
+      return True
+    else : 
+      return False
