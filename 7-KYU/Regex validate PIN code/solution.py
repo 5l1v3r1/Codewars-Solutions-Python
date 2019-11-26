121fb36b988403b9222c20852cb0107c77003ba5
Devansh Batham <devanshbatham009@gmail.com>
2019-11-26 19:27:36 +0530

    Create solution.py

---
+++ 7-KYU/Regex validate PIN code/solution.py
@@ -0,0 +1,10 @@
+'''
+challenge URL : https://www.codewars.com/kata/55f8a9c06c018a0d6e000132
+'''
+
+import re
+def validate_pin(pin):
+    if re.fullmatch("\d{4}|\d{6}", pin):
+        return True
+    else:
+        return False
