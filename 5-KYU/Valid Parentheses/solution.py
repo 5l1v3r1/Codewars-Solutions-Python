74a88ccd180f141c2e988a75779c31f308c9032f
Devansh Batham <devanshbatham009@gmail.com>
2019-11-26 19:27:32 +0530

    Create solution.py

---
+++ 5-KYU/Valid Parentheses/solution.py
@@ -0,0 +1,21 @@
+'''
+challenge URL : https://www.codewars.com/kata/52774a314c2333f0a7000688
+'''
+
+def valid_parentheses(string):
+    # your code here
+
+    stack = []
+    for s in string:
+        if(s == '('):
+            stack.append(s)
+        elif(s == ')'):
+            try:
+                stack.pop()
+            except:
+                return False
+
+    if(len(stack) == 0):
+        return True
+    else:
+        return False
